30dc9cdd05552a65520d9030fb431d5ea10d593d
Sayak Paul <spsayakpaul@gmail.com>
2020-08-23 13:57:25 +0530

    Added hyperparams explicitly

--- utils/architecture.py
+++ utils/architecture.py
@@ -3,26 +3,24 @@ from tensorflow.keras import models
 import tensorflow as tf
 
 def get_resnet_backbone():
-    base_model = tf.keras.applications.ResNet50(include_top=False,
-    	weights=None, input_shape=(None, None, 3))
-    base_model.trainabe = True
+	base_model = tf.keras.applications.ResNet50(include_top=False,
+		weights=None, input_shape=(None, None, 3))
+	base_model.trainabe = True
 
-    inputs = layers.Input((None, None, 3))
-    h = base_model(inputs, training=True)
-    h = layers.GlobalAveragePooling2D()(h)
+	inputs = layers.Input((None, None, 3))
+	h = base_model(inputs, training=True)
+	h = layers.GlobalAveragePooling2D()(h)
+	backbone = models.Model(inputs, h)
 
-    backbone = models.Model(inputs, h)
+	return backbone
 
-    return backbone
-
-def get_projection_prototype(prototype_dimension):
+def get_projection_prototype(dense_1=1024, dense_2=96, prototype_dimension):
 	inputs = layers.Input((2048, ))
-	projection_1 = layers.Dense(2048)(inputs)
+	projection_1 = layers.Dense(dense_1)(inputs)
 	projection_1 = layers.BatchNormalization()(projection_1)
 	projection_1 = layers.Activation("relu")(projection_1)
 
-	projection_2 = layers.Dense(128)(projection_1)
-
+	projection_2 = layers.Dense(dense_2)(projection_1)
 	projection_2_normalize = tf.math.l2_normalize(projection_2, axis=1, name='projection')
 
 	prototype = layers.Dense(prototype_dimension, use_bias=False, name='prototype')(projection_2_normalize)
